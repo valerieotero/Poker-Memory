98af884783b5517e16304d1d2c0a7be31b0ec1f2
Val <Val@10.31.47.5>
2017-11-18 18:40:09 -0400

    added constructer cause it made me

--- PokerMemory/src/FlushLevel.java
+++ PokerMemory/src/FlushLevel.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 
-public class FlushLevel {
+// FLUSH LEVEL: The goal is to find, on each turn, all (five) cards with the same rank
 
-	public static void main(String[] args) {
-		
+public class  FlushLevel extends RankTrioLevel  {
 
+	
+	protected FlushLevel(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) {
+		super(validTurnTime, mainFrame);
+		// TODO Auto-generated constructor stub
 	}
+
+	
+		
 
 }
