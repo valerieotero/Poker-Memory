ba8052b254bd66b7350533a909f30ccd369331dd
rober <rober@10.32.59.179>
2017-11-29 16:57:27 -0400

    Classes con los scores.

--- PokerMemory/src/EqualPairLevelWithScore.java
+++ PokerMemory/src/EqualPairLevelWithScore.java
@@ -2,14 +2,14 @@ import javax.swing.JFrame;
 
 public class EqualPairLevelWithScore extends EqualPairLevel
 {
-	private long scoreLabel = 0;
+	private long scoreLabel;
 	private final int LEVEL_INCREMENT = 50;
 	private final int PENALTY = 5;
 	
 	protected EqualPairLevelWithScore(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) 
 	{
 		super(validTurnTime, mainFrame);
-		
+		scoreLabel = 0;
 	}
 	
 	public void reset() 
@@ -26,4 +26,37 @@ public class EqualPairLevelWithScore extends EqualPairLevel
 	{
 		scoreLabel -= PENALTY;
 	}
+	
+	@Override
+	protected boolean turnUp(Card card) {
+		// the card may be turned
+		if(this.getTurnedCardsBuffer().size() < getCardsToTurnUp()) 
+		{
+			this.getTurnedCardsBuffer().add(card);
+			if(this.getTurnedCardsBuffer().size() == getCardsToTurnUp())
+			{
+				// there are two cards faced up
+				// record the player's turn
+				this.getTurnsTakenCounter().increment();
+				// get the other card (which was already turned up)
+				Card otherCard = (Card) this.getTurnedCardsBuffer().get(0);
+				// the cards match, so remove them from the list (they will remain face up)
+				if( otherCard.getNum() == card.getNum()) 
+				{
+					this.getTurnedCardsBuffer().clear();
+					this.increment();
+				}
+				// the cards do not match, so start the timer to turn them down
+				else 
+				{
+					this.getTurnDownTimer().start();
+					this.decrement();
+				}
+			}
+			return true;
+		}
+		// there are already the number of EasyMode (two face up cards) in the turnedCardsBuffer
+		return false;
+	}
+	
 }
--- PokerMemory/src/RankTrioLevelWithScore.java
+++ PokerMemory/src/RankTrioLevelWithScore.java
@@ -26,6 +26,42 @@ public class RankTrioLevelWithScore extends RankTrioLevel
 	{
 		scoreLabel -= PENALTY;
 	}
-
+	
+	public long getScoreLabel() 
+	{
+		return scoreLabel;
+	}
+	
+	@Override
+	protected boolean turnUp(Card card) {
+		// the card may be turned
+		if(this.getTurnedCardsBuffer().size() < getCardsToTurnUp()) 
+		{
+			// add the card to the list
+			this.getTurnedCardsBuffer().add(card);
+			if(this.getTurnedCardsBuffer().size() == getCardsToTurnUp())
+			{
+				// We are uncovering the last card in this turn
+				// Record the player's turn
+				this.getTurnsTakenCounter().increment();
+				// get the other card (which was already turned up)
+				Card otherCard1 = (Card) this.getTurnedCardsBuffer().get(0);
+				Card otherCard2 = (Card) this.getTurnedCardsBuffer().get(1);
+				if((card.getRank().equals(otherCard1.getRank())) && (card.getRank().equals(otherCard2.getRank()))) {
+					// Three cards match, so remove them from the list (they will remain face up)
+					this.getTurnedCardsBuffer().clear();
+					this.increment();
+				}
+				else
+				{
+					// The cards do not match, so start the timer to turn them down
+					this.getTurnDownTimer().start();
+					this.decrement();
+				}
+			}
+			return true;
+		}
+		return false;
+	}
 
 }
