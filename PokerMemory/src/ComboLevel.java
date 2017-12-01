88c49596c5912a66dc5ac8d5a75b6b73d98faf23
Valerie <valerie.otero2@upr.edu>
2017-12-01 16:48:43 -0400

    minor change

--- PokerMemory/src/ComboLevel.java
+++ PokerMemory/src/ComboLevel.java
@@ -5,75 +5,109 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 
-public class ComboLevel extends FlushLevel{
+public class ComboLevel extends StraightLevel{
 	
-	// Flush LEVEL: The goal is to find, on each turn, three cards with the same rank
-	private long currentScore=0;
-	protected ComboLevel(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) {
-		super(validTurnTime, mainFrame);
-		this.getTurnsTakenCounter().setDifficultyModeLabel("Flush Level");	
-		this.setCardsToTurnUp(5);
-		this.setCardsPerRow(10);
-		this.setRowsPerGrid(5);
-		this.getMainFrame().setScore(this.getLevelScore());
-	}	
-
-	@Override
-	protected boolean turnUp(Card card) {
-		// the card may be turned
-		if(this.getTurnedCardsBuffer().size() < getCardsToTurnUp()) 
-		{
-			super.turnUp(card);
-			// add the card to the list
-			this.getTurnedCardsBuffer().add(card);
-			if(this.getTurnedCardsBuffer().size() == getCardsToTurnUp())
-			{
-				// We are uncovering the last card in this turn
-				// Record the player's turn
-				this.getTurnsTakenCounter().increment();
-				// get the other card (which was already turned up)
-				Card otherCard1 = (Card) this.getTurnedCardsBuffer().get(0);
-				Card otherCard2 = (Card) this.getTurnedCardsBuffer().get(1);
-				Card otherCard3 = (Card) this.getTurnedCardsBuffer().get(2);
-				Card otherCard4 = (Card) this.getTurnedCardsBuffer().get(3);
-				if((card.getSuit().equals(otherCard1.getSuit())) && (card.getSuit().equals(otherCard2.getSuit())) && 
-						(card.getSuit().equals(otherCard3.getSuit())) && (card.getSuit().equals(otherCard4.getSuit()))) {
-					// Three cards match, so remove them from the list (they will remain face up)
-					currentScore += 700 + ConvertValue(card)+ConvertValue(otherCard1)+ConvertValue(otherCard2)+ConvertValue(otherCard3)+ConvertValue(otherCard4);
-					
-					this.getMainFrame().setScore(this.getLevelScore());
-					this.getTurnedCardsBuffer().clear();
-				}
-				else
-				{
-					currentScore -= 5;
-					this.getMainFrame().setScore(this.getLevelScore());
-					// The cards do not match, so start the timer to turn them down
-					this.getTurnDownTimer().start();
-				}
-			}
-			return true;
+		private long scoreLabel;
+		
+		protected ComboLevel(TurnsTakenCounterLabel validTurnTime, JFrame mainFrame) {
+			super(validTurnTime, mainFrame);
+			this.getTurnsTakenCounter().setDifficultyModeLabel("Combo Level");
+			this.setCardsToTurnUp(5);
+			this.setCardsPerRow(10);
+			this.setRowsPerGrid(5);
+			scoreLabel = 0;
 		}
-		return false;
-	}
-	public long getLevelScore(){
-		return currentScore;
-	}
-	
-//	@Override
-//	//Important for newGame method in MemoryGame and Memory Frame
-//	public String getMode() {
-//		// TODO Auto-generated method stub
-//		return "flush";
-//	}
-//	
-	
-	@Override
-	protected boolean isGameOver()
-	{		
-		for (int i =0; i< this.getGrid().size();i++)
-			if(!this.getGrid().get(i).isFaceUp()) return false;
+		
+				
+		@Override
+		protected void makeDeck() {
+			// In Combo level the grid consists of distinct cards, no repetitions
 
-		return true;
+			//back card
+			ImageIcon backIcon = this.getCardIcons()[this.getTotalCardsPerDeck()];
+
+			int cardsToAdd[] = new int[getRowsPerGrid() * getCardsPerRow()];
+			for(int i = 0; i < (getRowsPerGrid() * getCardsPerRow()); i++)
+			{
+				cardsToAdd[i] = i;
+			}
+
+			// randomize the order of the deck
+			this.randomizeIntArray(cardsToAdd);
+
+			// make each card object
+			for(int i = 0; i < cardsToAdd.length; i++)
+			{
+				// number of the card, randomized
+				int num = cardsToAdd[i];
+				// make the card object and add it to the panel
+				String rank = cardNames[num].substring(0, 1);
+				String suit = cardNames[num].substring(1, 2);
+				this.getGrid().add( new Card(this, this.getCardIcons()[num], backIcon, num, rank, suit));
+			}
+		}
+		public long getRankValue(Card theCard)
+		{
+			if(theCard.getRank().equals("a"))
+			{
+				return 20;
+			}
+			else if (theCard.getRank().equals("k"))
+			{
+				return 13;
+			}
+			else if (theCard.getRank().equals("q"))
+			{
+				return 12;
+			}
+			else if (theCard.getRank().equals("t"))
+			{
+				return 10;
+			}
+			else if (theCard.getRank().equals("j"))
+			{
+				return 11;
+			}
+			else
+			{
+				return Long.parseLong(theCard.getRank());
+			}
+		}
+		
+			
+		protected boolean turnUp(Card card) {
+			// the card may be turned
+			if(this.getTurnedCardsBuffer().size() < getCardsToTurnUp()) 
+			{
+				// add the card to the list
+				this.getTurnedCardsBuffer().add(card);
+				if(this.getTurnedCardsBuffer().size() == getCardsToTurnUp())
+				{
+					// We are uncovering the last card in this turn
+					// Record the player's turn
+					this.getTurnsTakenCounter().increment();
+					// get the other card (which was already turned up)
+					Card otherCard1 = (Card) this.getTurnedCardsBuffer().get(0);
+					Card otherCard2 = (Card) this.getTurnedCardsBuffer().get(1);
+					Card otherCard3 = (Card) this.getTurnedCardsBuffer().get(2);
+					Card otherCard4 = (Card) this.getTurnedCardsBuffer().get(3);
+					
+					if((card.getSuit().equals(otherCard1.getSuit())) && 
+							(card.getSuit().equals(otherCard2.getSuit())) &&
+							(card.getSuit().equals(otherCard3.getSuit())) &&
+							(card.getSuit().equals(otherCard4.getSuit())))
+					{
+					// Three cards match, so remove them from the list (they will remain face up)
+						this.scoreIncrement( card, otherCard1, otherCard2, otherCard3, otherCard4);
+						this.getMainFrame().setScore(this.getScoreLabel());
+						this.getTurnedCardsBuffer().clear();
+					}
+					
+				}
+				return true;
+			}
+	return false;
+		}
+		
 	}
-}
+
