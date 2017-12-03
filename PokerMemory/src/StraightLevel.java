cd3f428031904087931be1492bd97aeb96f832ce
rober <rober@136.145.116.181>
2017-12-03 17:36:17 -0400

    Straight is able to go from a to 5.

--- PokerMemory/src/StraightLevel.java
+++ PokerMemory/src/StraightLevel.java
@@ -1,5 +1,6 @@
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.swing.JFrame;
@@ -35,7 +36,7 @@ public class StraightLevel extends FlushLevel
 				Arrays.sort(straight);
 				
 
-				if(straight[0] == 10 && straight[1] == 11 && straight[2] == 12 && straight[3] == 13 && straight[4] == 20)
+				if((straight[0] == 10 && straight[1] == 11 && straight[2] == 12 && straight[3] == 13 && straight[4] == 20) || (straight[0] == 2 && straight[1] == 3 && straight[2] == 4 && straight[3] == 5 && straight[4] == 20))
 				{
 				// Three cards match, so remove them from the list (they will remain face up)
 					this.scoreLabel += 20 * 100 + 1000;
@@ -62,5 +63,40 @@ public class StraightLevel extends FlushLevel
 		}
 return false;
 	}
+		
+		@Override
+		protected boolean  isGameOver(){
+			ArrayList<Card>  daimonds = new ArrayList<Card>();
+			ArrayList<Card>  hearts = new ArrayList<Card>();
+			ArrayList<Card>  spades = new ArrayList<Card>();
+			ArrayList<Card>  crest = new ArrayList<Card>();
+
+			for(int j = 0;j < this.getGrid().size();j++) //Makes the arrays of the suits of Face Down Cards
+			{
+				if(this.getGrid().get(j).getSuit().equals("d") && this.getGrid().get(j).isFaceUp() == false)
+				{
+					daimonds.add(this.getGrid().get(j));
+				}
+				else if(this.getGrid().get(j).getSuit().equals("c") && this.getGrid().get(j).isFaceUp() == false)
+				{
+					crest.add(this.getGrid().get(j));
+				}
+				else if(this.getGrid().get(j).getSuit().equals("h") && this.getGrid().get(j).isFaceUp() == false)
+				{
+					hearts.add(this.getGrid().get(j));
+				}
+				else if(this.getGrid().get(j).getSuit().equals("s") && this.getGrid().get(j).isFaceUp() == false)
+				{
+					spades.add(this.getGrid().get(j));
+				}
+			}
+			
+			for (int i =0; i< this.getGrid().size();i++)
+				if(!this.getGrid().get(i).isFaceUp() && (daimonds.size() >= 5 || hearts.size() >= 5 || crest.size() >= 5 || spades.size() >= 5)) //The new condition states that there a need for at least one array of suits of the
+				{return false;}
+			
+
+			return true;
+		}
 
 }
