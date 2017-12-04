6101fff84fc4041ed930832642d0b14dbe9ed315
rober <rober@136.145.116.181>
2017-12-03 20:17:49 -0400

    StraightLevel ending done.

--- PokerMemory/src/StraightLevel.java
+++ PokerMemory/src/StraightLevel.java
@@ -64,38 +64,154 @@ public class StraightLevel extends FlushLevel
 return false;
 	}
 		
+		public void bubbleSort(ArrayList<Card> numArray) {
+
+		    int n = numArray.size();
+		    Card temp;
+
+		    for (int i = 0; i < n; i++) {
+		        for (int j = 1; j < (n - i); j++) {
+
+		            if (this.getRankValue(numArray.get(j - 1)) > this.getRankValue(numArray.get(j))) {
+		                temp = numArray.get(j - 1);
+		                numArray.set(j - 1, numArray.get(j));
+		                numArray.set(j, temp);
+		                //numArray[j - 1] = numArray[j];
+		                //numArray[j] = temp;
+		            }
+
+		        }
+		    }
+		}
+		public void checkDuplicates(ArrayList<Card> array)
+		{
+			this.bubbleSort(array);
+			long previousValue = this.getRankValue(array.get(0));
+			for(int i = 1; i < array.size(); i++)
+			{
+				if(this.getRankValue(array.get(i)) == previousValue)
+				{
+					array.remove(i);
+					i--;
+				}
+				else
+				{
+					previousValue = this.getRankValue(array.get(i));
+				}
+			}
+		}
+		
+//		@Override
+//		protected boolean  isGameOver(){
+//			ArrayList<Card>  daimonds = new ArrayList<Card>();
+//			ArrayList<Card>  hearts = new ArrayList<Card>();
+//			ArrayList<Card>  spades = new ArrayList<Card>();
+//			ArrayList<Card>  crest = new ArrayList<Card>();
+//
+//			for(int j = 0;j < this.getGrid().size();j++) //Makes the arrays of the suits of Face Down Cards
+//			{
+//				if(this.getGrid().get(j).getSuit().equals("d") && this.getGrid().get(j).isFaceUp() == false)
+//				{
+//					daimonds.add(this.getGrid().get(j));
+//				}
+//				else if(this.getGrid().get(j).getSuit().equals("c") && this.getGrid().get(j).isFaceUp() == false)
+//				{
+//					crest.add(this.getGrid().get(j));
+//				}
+//				else if(this.getGrid().get(j).getSuit().equals("h") && this.getGrid().get(j).isFaceUp() == false)
+//				{
+//					hearts.add(this.getGrid().get(j));
+//				}
+//				else if(this.getGrid().get(j).getSuit().equals("s") && this.getGrid().get(j).isFaceUp() == false)
+//				{
+//					spades.add(this.getGrid().get(j));
+//				}
+//			}
+//			
+//			for (int i =0; i< this.getGrid().size();i++)
+//				if(!this.getGrid().get(i).isFaceUp() && (daimonds.size() >= 5 || hearts.size() >= 5 || crest.size() >= 5 || spades.size() >= 5)) //The new condition states that there a need for at least one array of suits of the
+//				{return false;}
+//			
+//
+//			return true;
+//		}
 		@Override
 		protected boolean  isGameOver(){
-			ArrayList<Card>  daimonds = new ArrayList<Card>();
-			ArrayList<Card>  hearts = new ArrayList<Card>();
-			ArrayList<Card>  spades = new ArrayList<Card>();
-			ArrayList<Card>  crest = new ArrayList<Card>();
+			ArrayList<Card>  aToFive = new ArrayList<Card>();
+			ArrayList<Card>  twoToSix = new ArrayList<Card>();
+			ArrayList<Card>  threeToSeven = new ArrayList<Card>();
+			ArrayList<Card>  fourToEight = new ArrayList<Card>();
+			ArrayList<Card>  fiveToNine = new ArrayList<Card>();
+			ArrayList<Card>  sixToTen = new ArrayList<Card>();
+			ArrayList<Card>  sevenToJ = new ArrayList<Card>();
+			ArrayList<Card>  eightToQ = new ArrayList<Card>();
+			ArrayList<Card>  nineToK = new ArrayList<Card>();
+			ArrayList<Card>  aToK = new ArrayList<Card>();
+			
+			
 
 			for(int j = 0;j < this.getGrid().size();j++) //Makes the arrays of the suits of Face Down Cards
 			{
-				if(this.getGrid().get(j).getSuit().equals("d") && this.getGrid().get(j).isFaceUp() == false)
+				if(this.getRankValue(this.getGrid().get(j)) <= 5 || this.getRankValue(this.getGrid().get(j)) == 20)
 				{
-					daimonds.add(this.getGrid().get(j));
+				aToFive.add(this.getGrid().get(j));
 				}
-				else if(this.getGrid().get(j).getSuit().equals("c") && this.getGrid().get(j).isFaceUp() == false)
+				if(this.getRankValue(this.getGrid().get(j)) >= 2 && this.getRankValue(this.getGrid().get(j)) <= 6)
 				{
-					crest.add(this.getGrid().get(j));
+					twoToSix.add(this.getGrid().get(j));
 				}
-				else if(this.getGrid().get(j).getSuit().equals("h") && this.getGrid().get(j).isFaceUp() == false)
+				if(this.getRankValue(this.getGrid().get(j)) >= 3 && this.getRankValue(this.getGrid().get(j)) <= 7)
 				{
-					hearts.add(this.getGrid().get(j));
+					threeToSeven.add(this.getGrid().get(j));
 				}
-				else if(this.getGrid().get(j).getSuit().equals("s") && this.getGrid().get(j).isFaceUp() == false)
+				if(this.getRankValue(this.getGrid().get(j)) >= 4 && this.getRankValue(this.getGrid().get(j)) <= 8)
 				{
-					spades.add(this.getGrid().get(j));
+					fourToEight.add(this.getGrid().get(j));
+				}
+				if(this.getRankValue(this.getGrid().get(j)) >= 5 && this.getRankValue(this.getGrid().get(j)) <= 9)
+				{
+					fiveToNine.add(this.getGrid().get(j));
+				}
+				if(this.getRankValue(this.getGrid().get(j)) >= 6 && this.getRankValue(this.getGrid().get(j)) <= 10)
+				{
+					sixToTen.add(this.getGrid().get(j));
+				}
+				if(this.getRankValue(this.getGrid().get(j)) >= 7 && this.getRankValue(this.getGrid().get(j)) <= 11)
+				{
+					sevenToJ.add(this.getGrid().get(j));
+				}
+				if(this.getRankValue(this.getGrid().get(j)) >= 8 && this.getRankValue(this.getGrid().get(j)) <= 12)
+				{
+					eightToQ.add(this.getGrid().get(j));
+				}
+				if(this.getRankValue(this.getGrid().get(j)) >= 9 && this.getRankValue(this.getGrid().get(j)) <= 13)
+				{
+					nineToK.add(this.getGrid().get(j));
+				}
+				if((this.getRankValue(this.getGrid().get(j)) >= 10 && this.getRankValue(this.getGrid().get(j)) <= 13) || this.getRankValue(this.getGrid().get(j)) == 20)
+				{
+					aToK.add(this.getGrid().get(j));
 				}
 			}
 			
+			this.checkDuplicates(aToK);
+			this.checkDuplicates(nineToK);
+			this.checkDuplicates(eightToQ);
+			this.checkDuplicates(sevenToJ);
+			this.checkDuplicates(sixToTen);
+			this.checkDuplicates(fiveToNine);
+			this.checkDuplicates(fourToEight);
+			this.checkDuplicates(threeToSeven);
+			this.checkDuplicates(twoToSix);
+			this.checkDuplicates(aToFive);
+			
 			for (int i =0; i< this.getGrid().size();i++)
-				if(!this.getGrid().get(i).isFaceUp() && (daimonds.size() >= 5 || hearts.size() >= 5 || crest.size() >= 5 || spades.size() >= 5)) //The new condition states that there a need for at least one array of suits of the
+				if(!this.getGrid().get(i).isFaceUp() && (aToFive.size() >= 5 || twoToSix.size() >= 5 || threeToSeven.size() >= 5 
+				|| fourToEight.size() >= 5 || fiveToNine.size() >= 5 || sixToTen.size() >= 5  || sevenToJ.size() >= 5 || eightToQ.size() >= 5 
+				|| nineToK.size() <= 5) || aToK.size() <= 5) //The new condition states that there a need for at least one array of suits of the
 				{return false;}
 			
-
+			this.scoreLabel = 0;
 			return true;
 		}
 
